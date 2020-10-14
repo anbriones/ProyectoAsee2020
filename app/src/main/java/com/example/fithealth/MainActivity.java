e53b48547dd559ded13be4c466e6d8787e51db87
Ana <anbriones@alumnos.unex.es>
2020-10-14 18:33:54 +0200

    Pantalla home principio

--- app/src/main/java/com/example/fithealth/MainActivity.java
+++ app/src/main/java/com/example/fithealth/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.fithealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+    //Text view de la fecha
+        Date myDate = new Date();
+        String myString = DateFormat.getDateInstance().format(myDate);
+        DateFormat df = DateFormat.getDateInstance(DateFormat.LONG, Locale.ITALY);
+        try {
+            myDate = df.parse(myString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        TextView textView=findViewById(R.id.fecha);
+        textView.setText(myString);
     }
 }
